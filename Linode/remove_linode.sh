a8ad9ff8aff84d7ecdeb72e50046533842eeaebe
Nick Rimmer <nick.rimmer.2009@gmail.com>
2022-06-07 23:04:47 +0100

    Added workflow_dispatch

--- Linode/remove_linode.sh
+++ Linode/remove_linode.sh
@@ -1,3 +1,15 @@
-curl -H "Authorization: Bearer $TOKEN" \
+curl -k -H "Authorization: Bearer $TOKEN" \
     -X DELETE \
     https://api.linode.com/v4/linode/instances/UBULAB_SERVER
+
+    -H header
+    -k insecure
+    -X request command
+
+    $props = @{
+    Uri = 'https://api.linode.com/v4/linode/instances/UBULAB_SERVER'
+    Method = 'DELETE'
+    ContentType = 'application/json'
+    }
+$headers = @{Authorization = "Bearer $token"}
+$Response = Invoke-RestMethod @props -headers $headers 
